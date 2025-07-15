5a9caea1c69167d6aa828450c01a1448ff5e50a9
velaruneclassroom <aking1@tps501.org>
2025-07-15 14:41:40 -0500

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCOT9oajBB3jIhPMjshdIbjfpssTllFijI",
+  authDomain: "velarune-rpg.firebaseapp.com",
+  projectId: "velarune-rpg",
+  storageBucket: "velarune-rpg.firebasestorage.app",
+  messagingSenderId: "832434090158",
+  appId: "1:832434090158:web:984c3228a6495ce2c1fd3f"
+};
