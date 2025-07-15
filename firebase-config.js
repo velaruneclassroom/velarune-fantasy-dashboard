5f6b13b122369ec5e91012dd2951c3a028ca68a8
velaruneclassroom <aking1@tps501.org>
2025-07-15 15:09:11 -0500

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,8 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyCOT9oajBB3jIhPMjshdIbjfpssTllFijI",
   authDomain: "velarune-rpg.firebaseapp.com",
   projectId: "velarune-rpg",
   storageBucket: "velarune-rpg.firebasestorage.app",
   messagingSenderId: "832434090158",
-  appId: "1:832434090158:web:984c3228a6495ce2c1fd3f"
+  appId: "1:832434090158:web:312f11e71dd830d8c1fd3f"
 };
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
